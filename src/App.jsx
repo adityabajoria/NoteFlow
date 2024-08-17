9d68132791ba08c7f0a5fd00df49b29a7154b87a
Aditya Bajoria <aadityaudel.edu>
2024-08-17 16:55:00 +0530

    more changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,67 @@
-import "./App.css";
+import NotesList from "./components/NotesList";
+import SearchBar from "./components/SearchBar";
+import { nanoid } from "nanoid";
+import { useState, useEffect } from "react";
 
-export default function App() {
-  return <div>NoteFlow</div>;
-}
+const App = () => {
+  /*
+ Additional Features to add in the App:
+ - Markdown Support (bold, italic, headers) - use react-markdown lib
+ - Use Google Fonts for text
+ - Drag-drop notes (react-beautiful-dnd)
+ - Note Sharing (enable a 'sharing' button - share using email)
+ - Note Archive
+ */
+  const [notes, setNotes] = useState([]);
+  const [search, setSearch] = useState("");
+  const [darkMode, setDarkMode] = useState(false);
+
+  useEffect(() => {
+    const savedNotes = JSON.parse(localStorage.getItem("notes-data"));
+    if (savedNotes) {
+      setNotes(savedNotes);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("notes-data", JSON.stringify(notes));
+    localStorage.setItem("archieved-notes-data", JSON.stringify);
+  }, [notes]);
+
+  const addNote = (text) => {
+    const date = new Date();
+    const newNote = {
+      id: nanoid(),
+      text: text,
+      date: date.toLocaleString(),
+    };
+    const newNotes = [...notes, newNote];
+    setNotes(newNotes);
+  };
+
+  const deleteNote = (id) => {
+    const updatedNotes = notes.filter((note) => note.id !== id);
+    setNotes(updatedNotes);
+  };
+
+  return (
+    <div className={`${darkMode && "dark-mode"}`}>
+      <div className="container">
+        <h1 className="header">Notes App</h1>
+        <button onClick={() => setDarkMode((prev) => !prev)} className="save">
+          {darkMode ? "lightMode" : "darkMode"}
+        </button>
+        <SearchBar onSearch={setSearch} /> {/* Update prop name */}
+        <NotesList
+          notes={notes.filter((note) =>
+            note.text.toLowerCase().includes(search)
+          )}
+          AddNote={addNote}
+          Delete={deleteNote}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default App;
